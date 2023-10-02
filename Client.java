64241ecf754b806a986d731a91195edf5a8d7adf
ABHAY <107785681+abhay8298@users.noreply.github.com>
2023-10-02 18:06:12 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+
+public class Client {
+    Socket socket;
+
+    BufferedReader br;
+    PrintWriter out;
+
+    public Client() {
+        try {
+            System.out.println("Sending request to server...");
+            socket = new Socket("127.0.0.1", 7777);
+           
+            System.out.println("Connected Established with server..");
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            startReading();
+            startWriting();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void startReading() {
+        Runnable r1 = () -> {
+            System.out.println("reader started..");
+            while (true) {
+                try {
+                    String msg = br.readLine();
+                    if (msg.equals("exit")) {
+                        System.out.println("Server terminated the chat ..");
+                        break;
+                    }
+                    System.out.println("Server  : " + msg);
+
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        };
+          new Thread(r1).start();
+    }
+
+    public void startWriting() {
+        Runnable r2 = () -> {
+            System.out.println("writer started..");
+            while (true) {
+                try {
+                    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                    String content  = br1.readLine();
+                    out.println(content);
+                    out.flush();
+                } catch (Exception e) {
+                   e.printStackTrace();
+                }
+            }
+
+        };
+
+        new Thread(r2).start();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("this is client ..");
+        new Client();
+    }
+
+}
